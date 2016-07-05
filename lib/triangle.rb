29aab135ec0e8adb2d3ed9a0a25ea9cba8cb0086
aehmt <calisahmet@gmail.com>
2016-07-05 18:14:39 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_reader :a , :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def err
+    lengths = [a, b, c].sort
+    if  (a == 0 || b == 0 || c == 0) || (a < 0 || b < 0 || c < 0)
+      raise TriangleError
+    elsif lengths[0] + lengths[1] <= lengths[2]
+      raise TriangleError
+    end
+  end
+ 
+  def kind
+    err
+      if c == b && b == a
+        return :equilateral
+      elsif a == c  || b == c || a == b
+        return :isosceles
+      elsif c != b && b != a && a != c
+        return :scalene
+      end
+    
+  end
+
+
+
+
+end
+
+
+class TriangleError < StandardError
 end
