8855e8c8913e1e72e2d8621678e517c5ee772fdc
aehmt <calisahmet@gmail.com>
2016-07-05 19:55:23 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,11 +10,7 @@ class Triangle
   
   def err
     lengths = [a, b, c].sort
-    if  (a == 0 || b == 0 || c == 0) || (a < 0 || b < 0 || c < 0)
-      raise TriangleError
-    elsif lengths[0] + lengths[1] <= lengths[2]
-      raise TriangleError
-    end
+    raise TriangleError if (a <= 0 || b <= 0 || c <= 0) || lengths[0] + lengths[1] <= lengths[2]
   end
  
   def kind
@@ -23,17 +19,12 @@ class Triangle
         return :equilateral
       elsif a == c  || b == c || a == b
         return :isosceles
-      elsif c != b && b != a && a != c
+      else
         return :scalene
       end
-    
   end
-
-
-
-
 end
 
-
 class TriangleError < StandardError
+
 end
