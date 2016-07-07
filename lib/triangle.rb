ad294c25be9eb02eaf6d7f474c709034176806a6
Raycent Tan <ltan2@babson.edu>
2016-07-07 19:08:32 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,27 +2,24 @@ class Triangle
   attr_reader :sides
 
   def initialize(a, b, c)
-    @sides = [a, b, c].sort #sides can be [s, m, l], [s, s, l], [s, l, l], [s, s, s]
+    @sides = [a, b, c].sort
   end
 
   def kind
     validate_triangle
 
-    if sides[0] == sides[1] || sides[1] == sides[2] #[s, s, l], [s, l, l], [s, s, s]
-      if sides[0] == sides[2] #[s, s, s]
-        :equilateral
-      else
-        :isosceles
-      end
-
-    else #[s, m, l]
+    if sides[0] == sides[2]
+      :equilateral
+    elsif sides[0] == sides[1] || sides[1] == sides[2]
+      :isosceles
+    else
       :scalene
     end
   end
 
   private
   def validate_triangle
-    raise TriangleError if sides[0] + sides[1] <= sides[2] # [0] & [1] are the smallest, so it also cover the zero and negative scenarios.
+    raise TriangleError if sides[0] + sides[1] <= sides[2]
   end
 end
 
