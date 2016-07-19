fd74d8d749189e133d999098916c583cb3c1b7a3
satub <satu.barnhill@gmail.com>
2016-07-19 14:15:16 -0400

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize (side1, side2, side3)
+    @a = side1
+    @b = side2
+    @c = side3
+  end
+
+  def valid?
+    (@a > 0 && @b > 0 && @c > 0) && (@a + @b > @c && @a + @c > @b && @b + @c > @a)
+  end
+
+  def kind
+  #  binding.pry
+    if !valid?
+        raise TriangleError
+     else
+      if @a == @b && @b == @c
+        :equilateral
+      elsif @a == @b || @b == @c || @a == @c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "Invalid input. Cannot form a triangle with the given measurements."
+  end
 end
