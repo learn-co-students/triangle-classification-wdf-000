a27079b03afac6734f2fa345d60d3a72873bb1fd
paige11 <ellis.paige.v@gmail.com>
2016-07-26 12:00:05 -0400

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @sides = Array.new.push(@side_1, @side_2, @side_3)
+  end
+
+  def kind
+    if (@side_1 + @side_2 > @side_3 && @side_1 + @side_3 > @side_2 && @side_2 + @side_3 > @side_1) && (@sides.select {|side| side < 0} == [])
+      if @side_1 == @side_2 && @side_2 == @side_3
+        :equilateral
+      elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+        :isosceles
+      else
+        :scalene
+      end
+    else
+        raise TriangleError
+    end
+  end
+
+ 
+end
+
+class TriangleError < StandardError
 end
